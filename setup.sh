698a76f3a691e749d8c614f2dbfdae3f8ae74278
Pete Keen <pete@petekeen.net>
2022-09-16 14:19:54 -0400

    add tailscale

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ EOF
 
 apk update
 
-apk add docker qemu-guest-agent
+apk add docker qemu-guest-agent tailsacle
 
 mkdir -p /etc/docker
 
@@ -33,8 +33,15 @@ rc-service docker start
 rc-update add qemu-guest-agent
 rc-service qemu-guest-agent start
 
+rc-update add tailscale
+rc-service tailscale start
+
 mkdir -p /var/lib/docker/stack_configs
+
+tailscale up --advertise-tags tag:server
+
+docker network inspect tailnet > /dev/null 2>&1 || docker network create -d bridge -o com.docker.network.bridge.host_binding_ipv4=$(tailscale ip | head -n1) tailnet
 
 docker login ghcr.io
 
-docker run --init --restart=unless-stopped -d -it -v /var/lib/docker/stack_configs:/configs -v /var/run/docker.sock:/var/run/docker.sock -v /etc/hostname:/app/hostname:ro -e CONFIGS_DIR=/var/lib/docker/stack_configs --name dockerstack-root -l dockerstack-root ghcr.io/keenfamily-us/dockerstack-keenland:main
+docker inspect dockerstack-root > /dev/null 2>&1 || docker run --init --restart=unless-stopped -d -it -v /var/lib/docker/stack_configs:/configs -v /var/run/docker.sock:/var/run/docker.sock -v /etc/hostname:/app/hostname:ro -e CONFIGS_DIR=/var/lib/docker/stack_configs --name dockerstack-root -l dockerstack-root ghcr.io/keenfamily-us/dockerstack-keenland:main
